c967fbcd8f2c84b20585e28309e09aaf351efc4b
Nierot <56843328+Nierot@users.noreply.github.com>
2020-08-13 11:53:42 +0200

    routing table

--- app.js
+++ app.js
@@ -4,43 +4,36 @@ const proxy = httpProxy.createProxyServer({});
 const { verbose, info, error } = require('nielog').Log;
 const fs = require('fs');
 const path = require('path');
+const routes = require('./routes.json');
 
 http.createServer((req, res) => {
-
-
-    switch (req.url) {
-        case '/':
-            res.writeHead(200);
-            return res.end('oof');
-        case '/proxy':
-            return proxy.web(req, res, {
-                target: 'http://localhost:8079/',
-                ws: true
-            });
-        default:
-            var filePath = path.join(__dirname, 'static/404.html');
-            var stat = fs.statSync(filePath);
-        
-            res.writeHead(404, {
-                'Content-Type': 'text/html',
-                'Content-Length': stat.size
-            })
-        
-            var readStream = fs.createReadStream(filePath);
-            readStream.pipe(res);
-
-    }
+    let url = req.url;
+    let resolved = false;
+    verbose(`${req.method}: ${url}`);
+    if (url === '/') {
+        res.writeHead(200);
+        res.end('oof');
+    } else {
+        routes.forEach(route => {
+            //verbose(route.url, url, route.url.startsWith(url))
+            if (url.startsWith(route.url)) {
+                if (!resolved) proxy.web(req, res, { target: route.route });
+                resolved = true;
+            } 
+        })
+        if (!resolved) sendFile(path.join(__dirname, 'static/404.html'), 404, res);
+    } 
 }).listen(8001, () => console.log('Listening on http://localhost:8001'));
 
-proxy.on('error', (err, req, res) => {
-    var filePath = path.join(__dirname, 'static/500.html');
-    var stat = fs.statSync(filePath);
+proxy.on('error', (err, req, res) => sendFile(path.join(__dirname, 'static/500.html'), 500, res));
 
-    res.writeHead(500, {
+function sendFile(path, status, res) {
+    var stat = fs.statSync(path);
+    res.writeHead(status, {
         'Content-Type': 'text/html',
         'Content-Length': stat.size
     })
 
-    var readStream = fs.createReadStream(filePath);
+    var readStream = fs.createReadStream(path);
     readStream.pipe(res);
-});
+}
